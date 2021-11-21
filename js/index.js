2e010da3634dc20cf26cba7811a89023ca0e9e3e
VictorAdamus <94673707+VictorAdamus@users.noreply.github.com>
2021-11-22 00:18:50 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -15,16 +15,41 @@ let choiceTwo=document.querySelector('.choice2');
 for(let choice of choices){
     choice.addEventListener('click', function (e){
         if (e.target.textContent==="Yearly"){
-            priceOne.textContent="$20";
-            priceTwo.textContent="$40";
+            priceOne.textContent="29";
+            priceTwo.textContent="49";
             this.classList.add("activated");
             choiceTwo.classList.remove("activated");
         }
         if(e.target.textContent==="Monthly"){
-            priceOne.textContent="$29";
-            priceTwo.textContent="$49";
+            priceOne.textContent=Number(priceOne.textContent)*2;
+            priceTwo.textContent=Number(priceTwo.textContent)*2;
             this.classList.add("activated");
             choiceOne.classList.remove("activated");
+
+        }
+    })
+}
+let pricePlans=document.querySelectorAll('.btn-plan');
+let nameFree=document.querySelector('.name-start');
+let namePrem=document.querySelector('.name-prem');
+let nameEnter=document.querySelector('.name-enter');
+let priceFree=document.querySelector('.price-free');
+for (let pricePlan of pricePlans){
+    pricePlan.addEventListener('click', function (e){
+        if(e.target.className ==="btn-plan Starter"){
+            console.log('Название тарифа: '+ nameFree.textContent);
+            console.log('Цена за месяц: '+ priceFree.textContent);
+            console.log('Цена за год: '+ priceFree.textContent);
+        }
+        if(e.target.className ==="btn-plan Premium"){
+            console.log('Название тарифа: '+ namePrem.textContent);
+            console.log('Цена за месяц: '+ priceOne.textContent+'$');
+            console.log('Цена за год: '+ Number(priceOne.textContent)*12+'$');
+        }
+        if(e.target.className ==="btn-plan Enterprise"){
+            console.log('Название тарифа: '+ nameEnter.textContent);
+            console.log('Цена за месяц: '+ priceTwo.textContent+'$');
+            console.log('Цена за год: '+ Number(priceTwo.textContent)*12+'$');
         }
     })
 }
