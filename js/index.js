49b6842a88bae2e172a98a7e3b7eb9304862cb50
VictorAdamus <94673707+VictorAdamus@users.noreply.github.com>
2021-11-21 20:10:02 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -7,6 +7,30 @@ startSmooth.addEventListener('click', startSmoothClick);
 
 
 
+let priceOne=document.querySelector('.price1');
+let priceTwo=document.querySelector('.price2');
+let choices=document.querySelectorAll('.choice');
+let choiceOne=document.querySelector('.choice1');
+let choiceTwo=document.querySelector('.choice2');
+for(let choice of choices){
+    choice.addEventListener('click', function (e){
+        if (e.target.textContent==="Yearly"){
+            priceOne.textContent="$20";
+            priceTwo.textContent="$40";
+            this.classList.add("activated");
+            choiceTwo.classList.remove("activated");
+        }
+        if(e.target.textContent==="Monthly"){
+            priceOne.textContent="$29";
+            priceTwo.textContent="$49";
+            this.classList.add("activated");
+            choiceOne.classList.remove("activated");
+        }
+    })
+}
+
+
+
 
 
 
